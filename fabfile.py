0b487a763a926cfd299bad72b650214ec58d9ecd
fimanishi <fimanishi@gmail.com>
2017-08-03 14:53:59 -0500

    deployment files

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+from fabric.api import run, env, sudo, cd, prefix
+
+env.hosts = ['104.131.83.165']
+env.user = 'santo'
+
+DIR = '/home/santo/santo_banquete'
+VENV = 'source /home/santo/.virtualenvs/santo_banquete/bin/activate && source SECRETS.ENV'
+
+def start ():
+  with cd(DIR):
+    with prefix(VENV):
+      run('pm2 start uwsgi -- --ini uwsgi.ini > start.log')
+
+def stop ():
+  run('pm2 stop all > stop.log')
+
+def deploy ():
+  with cd(DIR):
+    run('git pull')
+
+    with prefix(VENV):
+      run('pip install -r requirements.txt  > install.log')
+      run('python manage.py migrate')
+
+    run('pm2 restart all > restart.log')
+
+def hello ():
+  print("Hello")
--- santo/santo/settings.py
+++ santo/santo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.0.0.5", "localhost"]
+ALLOWED_HOSTS = ["104.131.83.165", "localhost"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
